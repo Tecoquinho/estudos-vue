5814d213f2b101591114a6371d145cc853e6720e
Matheus <matheus.lage@icloud.com>
2023-12-28 14:53:40 -0300

    Numeros Loto finalizado

--- Loto/app.js
+++ Loto/app.js
@@ -1,16 +1,20 @@
 const app = Vue.createApp({
-    el: '#listaExample',
-  data() {
-    return {
-        items: [
-        { message: 'Vue' },
-        { message: 'JavaScript' },
-        { message: 'Texto 4' },
-        { message: 'Texto 5' },
-        { message: 'Lorem ipsum' }
-
-    ]
+    data(){
+      return{
+      numeros: [],
       }
-  }
+    },
+    methods:{
+      gerarNumerosAleatorios() {
+        this.numeros = [];
+        for (let i = 0; i < 6; i++) {
+          let numero = Math.floor(Math.random() * 99) + 1;
+          while (this.numeros.includes(numero)) {
+            numero = Math.floor(Math.random() * 99) + 1;
+          }
+          this.numeros.push(numero);
+        }
+      }
+    }
 });
-app.mount('#events'); //Connecting Vue to element wich id is user-goal
+app.mount('#loto'); //Connecting Vue to element wich id is user-goal
